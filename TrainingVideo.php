8bed6d2c3b2f1f678ba7891eca8e6854df0e5a5d
Rebecca99 <rlpassa99@gmail.com>
2017-01-30 22:37:41 -0500

    Update TrainingVideo.php

--- TrainingVideo.php
+++ TrainingVideo.php
@@ -4,7 +4,7 @@
 $botToken = "269724202:AAFpLIyZGM56xd6V8lbHeK1D-Jlz9Iv9CmE";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_getcontents($website."/getupdates");
+$update = file_get_contents($website."/getupdates");
 
 print_r($update);
 
